95748877058c68873215aa9deaf1ee7130484c80
Silvio Wanderson Florentino <professorsilvioflorentino@yahoo.com.br>
2022-03-29 15:27:51 -0300

    novos dados

--- model/Constants.php
+++ model/Constants.php
@@ -1,6 +1,6 @@
 <?php
 
-define('DB_HOST','localhost');
-define('DB_USER','root');
-define('DB_PASS','');
+define('DB_HOST','silvioflorentino.mysql.database.azure.com');
+define('DB_USER','silvioflorentino@silvioflorentino');
+define('DB_PASS','1978@|Silvio');
 define('DB_NAME','hortfruitbd');
