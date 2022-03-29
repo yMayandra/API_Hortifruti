9b3ded5e747140d775bdd29675ec17bb4f45f3a5
Mayandra Vasconcelos <86685244+yMayandra@users.noreply.github.com>
2022-03-29 16:09:57 -0300

    Update Constants.php

--- model/Constants.php
+++ model/Constants.php
@@ -1,6 +1,6 @@
 <?php
 
-define('DB_HOST','silvioflorentino.mysql.database.azure.com');
-define('DB_USER','silvioflorentino@silvioflorentino');
-define('DB_PASS','1978@|Silvio');
-define('DB_NAME','hortfruitbd');
+define('DB_HOST','meisz.mysql.database.azure.com');
+define('DB_USER','mei@meisz');
+define('DB_PASS','Vasconcelosmota123');
+define('DB_NAME','fruta_tb');
